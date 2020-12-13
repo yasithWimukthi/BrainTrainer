42902e59164bc8a084e3210c119ee1dbc2674b8a
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-13 11:49:35 +0530

    Create Interface

--- app/src/main/java/com/androidstudio/kotlin/braintrainer/MainActivity.java
+++ app/src/main/java/com/androidstudio/kotlin/braintrainer/MainActivity.java
@@ -3,12 +3,26 @@ package com.androidstudio.kotlin.braintrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
+
+    public void start(View view){
+        startButton.setVisibility(view.INVISIBLE);
+    }
+
+    public void chooseAnswer(View view){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.startButton);
     }
 }
